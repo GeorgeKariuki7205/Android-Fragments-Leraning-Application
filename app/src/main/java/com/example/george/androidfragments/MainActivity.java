123a550c95a35d97075d8382d347f34626957bea
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-07-21 18:21:09 +0300

    Added Second Layout

--- app/src/main/java/com/example/george/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/george/androidfragments/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.george.androidfragments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //The method that is used to launch the new activity.
+
+    public  void launchDetails(View view){
+
+        CharSequence text = "The launch button is clicked";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast =  Toast.makeText(this,text,duration);
+        toast.show();
+
     }
 }
