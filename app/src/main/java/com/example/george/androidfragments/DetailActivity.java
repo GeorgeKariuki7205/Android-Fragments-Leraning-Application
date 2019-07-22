5839dbb06ded7957a19b41c1186a5a9440028815
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-07-22 09:25:31 +0300

    Added back navigation and the actionbar

--- app/src/main/java/com/example/george/androidfragments/DetailActivity.java
+++ app/src/main/java/com/example/george/androidfragments/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.george.androidfragments;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Adding the toolbar.
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //setting the up navigation functionality of the application.
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/george/androidfragments/MainActivity.java
+++ app/src/main/java/com/example/george/androidfragments/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.george.androidfragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+//import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adding the appropriate toolbar.
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
     }
 
     //The method that is used to launch the new activity.
@@ -18,12 +27,15 @@ public class MainActivity extends AppCompatActivity {
 
     public  void launchDetails(View view){
 
-        CharSequence text = "The button is clicked "+number+" times";
-        int duration = Toast.LENGTH_SHORT;
+//        CharSequence text = "The button is clicked "+number+" times";
+//        int duration = Toast.LENGTH_SHORT;
+//
+//        Toast toast =  Toast.makeText(this,text,duration);
+//        toast.show();
+//        number++;
 
-        Toast toast =  Toast.makeText(this,text,duration);
-        toast.show();
-        number++;
+        Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+        startActivity(intent);
 
     }
 }
