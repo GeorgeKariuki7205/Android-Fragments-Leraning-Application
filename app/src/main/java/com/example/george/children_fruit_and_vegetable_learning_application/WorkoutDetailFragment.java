e9118b459e959167459df1b083f961ea5d76dccc
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-08-03 14:59:50 +0300

    Fragments Added.

---
+++ app/src/main/java/com/example/george/children_fruit_and_vegetable_learning_application/WorkoutDetailFragment.java
@@ -0,0 +1,31 @@
+package com.example.george.children_fruit_and_vegetable_learning_application;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.george.androidfragments.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+}
